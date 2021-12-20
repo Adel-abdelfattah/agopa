cbf6d60d9b7e86fb9f37edd16eb30a0354525c3f
Adel-abdelfattah <90777666+Adel-abdelfattah@users.noreply.github.com>
2021-12-20 15:22:15 +0200

    agopa

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+var wid = 992;
+$(".list-menu").hover(function(){
+  if( $(window).innerWidth() >= wid) { 
+    $(this).children('.dropdown-menu').stop(true, false, true).slideToggle(350).delay(3000);
+  };
+});
+
+$(".nav-link").click(function(){
+  if( $(window).innerWidth() < wid) { 
+    $(this).children('.dropdown-menu').stop(false, false, false).slideToggle(350).delay(3000);
+  }
+});
+
+
+$(".search").click(function(){
+  $(".dropdown-search").toggleClass("ddd");
+  return false;
+});
+
+
+
+$(document).click(function() {
+  $(".dropdown-search").removeClass("ddd");
+}); 
+
+$(".dropdown-search").click(function(e) {
+  e.stopPropagation();
+});
+
+$("#right-form").click(function(){
+  $(".overlay").show();
+  $(".right-form").addClass("gh");
+});
+
+$(".overlay, .rotate").click(function() {
+  $(".overlay").hide();
+  $(".right-form").removeClass("gh");
+});
+
+
+$(window).on("scroll", function(){
+  var winTop = $(window).scrollTop();
+  if (winTop >=140){
+    $(".sticky-header").addClass("sticky-header-1");
+  }else{
+    $(".sticky-header").removeClass("sticky-header-1");
+  }
+});
+
